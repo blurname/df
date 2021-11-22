9c1d31151d030de1c728bbba3461d391d39ef2d9
blurname <naughtybao@outlook.com>
2021-11-23 02:10:25 +0800

    udpate: nix lsp related

--- system/configuration.nix
+++ system/configuration.nix
@@ -6,7 +6,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -14,20 +15,20 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-   #networking.hostName = "nixos"; # Define your hostname.
-   #networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  #networking.hostName = "nixos"; # Define your hostname.
+  #networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
   networking = {
-	  hostName = "nixos";
-	  useDHCP = true;
-#	  interfaces={
-#	  enp0s3.ip4=[{
-#		  address = "192.168.1.2";
-#		  prefixLength = 28;
-#	  }];
-#	  };
+    hostName = "nixos";
+    useDHCP = true;
+    #    interfaces={
+    #    enp0s3.ip4=[{
+    #      address = "192.168.1.2";
+    #      prefixLength = 28;
+    #    }];
+    #    };
   };
   # Set your time zone.
-   time.timeZone = "Asia/Shanghai";
+  time.timeZone = "Asia/Shanghai";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -35,8 +36,8 @@
   #networking.useDHCP = true;
   #networking.interfaces.enp0s3.useDHCP = true;
   services.openssh = {
-	  enable = true;
-	  passwordAuthentication = true;
+    enable = true;
+    passwordAuthentication = true;
   };
 
   # Configure network proxy if necessary
@@ -44,64 +45,80 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-   i18n.defaultLocale = "en_US.UTF-8";
-   console = {
-     font = "Lat2-Terminus16";
-     keyMap = "us";
-   };
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
 
   # Enable the X11 windowing system.
-   #services.xserver.enable = true;
-   #services.xserver.windowManager.leftwm.enable = true;
+  #services.xserver.enable = true;
+  #services.xserver.windowManager.leftwm.enable = true;
 
   # Configure keymap in X11
-   #services.xserver.layout = "us";
+  #services.xserver.layout = "us";
 
   # Enable sound.
-   #sound.enable = true;
-   #hardware.pulseaudio.enable = true;
-#   home-manager.users.bl ={pkgs,...}:{
-#	home.pacages = with pkgs;[
-#		atool httpie
-#	];
-   #	programs.bash.enable = true;
-   #};
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+  #   home-manager.users.bl ={pkgs,...}:{
+  #  home.pacages = with pkgs;[
+  #    atool httpie
+  #  ];
+  #  programs.bash.enable = true;
+  #};
 
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.bl = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     password="a";
-   };
-   users.defaultUserShell=pkgs.elvish;
-   environment.systemPackages = with pkgs; [
-	   vim git 
-		   starship exa bat alacritty ripgrep fd fzf starship gcc gitui
-		   wget
-		   picom firefox 
-		   zsh elvish
-		   rustup
-		   python39
-		   nodejs-16_x yarn
-   ];
-   
-   programs.neovim={
-   	enable = true;
-	defaultEditor = true;
-	   configure = {
-		customRC = builtins.readFile /home/bl/.config/nvim/init.vim;
-		   packages.myVimPackage = with pkgs.vimPlugins;{
-			   start = [packer-nvim];
-		   };
-	   };
-   };
+  users.users.bl = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    password = "a";
+  };
+  users.defaultUserShell = pkgs.elvish;
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    starship
+    exa
+    bat
+    alacritty
+    ripgrep
+    fd
+    fzf
+    starship
+    gcc
+    gitui
+    wget
+    picom
+    firefox
+    zsh
+    elvish
+    rustup
+    python39
+    nodejs-16_x
+    yarn
+    rnix-lsp
+    nixpkgs-fmt
+    zellij
+  ];
 
-   #services.picom.inactiveOpacity = 0.7;
-   #services.picom.opacityRules = [
-#	"100:class_g = 'Alacritty'"
-   #];
-   nix.binaryCaches = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" "https://mirrors.ustc.edu.cn/nix-channels/store" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    configure = {
+      customRC = builtins.readFile /home/bl/.config/nvim/init.vim;
+      packages.myVimPackage = with pkgs.vimPlugins;{
+        start = [ packer-nvim ];
+      };
+    };
+  };
+
+  #services.picom.inactiveOpacity = 0.7;
+  #services.picom.opacityRules = [
+  #  "100:class_g = 'Alacritty'"
+  #];
+  nix.binaryCaches = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" "https://mirrors.ustc.edu.cn/nix-channels/store" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
 
   # List services that you want to enable:
 
@@ -123,4 +140,3 @@
   system.stateVersion = "unstable"; # Did you read the comment?
 
 }
-
