65272751db9ff4363a596cc13b817837588da1b4
blurname <naughtybao@outlook.com>
2022-10-25 23:43:02 +0800

    ADD: hyprland

--- 1-nixos/flake.nix
+++ 1-nixos/flake.nix
@@ -10,13 +10,22 @@
 		#url ="github:nix-community/neovim-nightly-overlay";
 		#inputs.nixpkgs.follows = "nixpkgs";
 		#};
+		hyprland = {
+      		url = "github:hyprwm/Hyprland";
+      		# build with your own instance of nixpkgs
+      		inputs.nixpkgs.follows = "nixpkgs";
+    };
 	};
-	outputs = inputs@{self,nixpkgs,...}:{
+	outputs = inputs@{self,nixpkgs,hyprland,...}:{
 		nixosConfigurations={
 			nyx = nixpkgs.lib.nixosSystem{
 				system ="x86_64-linux";
       			specialArgs = inputs;
-				modules=[./configuration.nix];
+				modules=[
+					./configuration.nix
+					hyprland.nixosModules.default
+        			{ programs.hyprland.enable = true; }
+				];
 			};
 		};
 	};
