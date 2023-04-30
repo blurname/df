a3ec121cbf1ca2b182ae59dbb1d10a6ade3e545b
blurname <naughtybao@outlook.com>
2023-05-01 00:18:59 +0800

    BUMP: lock

--- 1-nixos/flake.nix
+++ 1-nixos/flake.nix
@@ -4,10 +4,10 @@
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url="github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #home-manager = {
+      #url="github:nix-community/home-manager";
+      #inputs.nixpkgs.follows = "nixpkgs";
+    #};
 
     #neovim-nightly ={
       #url ="github:nix-community/neovim-nightly-overlay";
--- 1-nixos/sub/software/gui.nix
+++ 1-nixos/sub/software/gui.nix
@@ -17,7 +17,8 @@
       obsidian
       libsForQt5.qt5ct
       syncthing
-      fcitx5-gtk fcitx5-rime rime-data
+      fcitx5-gtk fcitx5-rime 
+      #rime-data
       libsForQt5.fcitx5-qt
       pcmanfm
       trayer nm-tray syncthingtray 
@@ -35,10 +36,7 @@
 
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.enableRimeData = true;
-    fcitx.engines = with pkgs.fcitx-engines;[
-      rime
-    ];
+    #fcitx5.enableRimeData = true;
     fcitx5.addons = with pkgs;
     [
         fcitx5-chinese-addons
