3ff2755bf425964349ebc7b324956fba59c904ee
blurname <naughtybao@outlook.com>
2023-05-14 11:38:09 +0800

    CHG: nix: config

--- 1-nixos/flake.nix
+++ 1-nixos/flake.nix
@@ -1,5 +1,22 @@
 {
   description="blurname's flake";
+    nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   inputs={
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
