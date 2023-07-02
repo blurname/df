f9a07625fdaa80ba022fc0c3c2a9a705081bc641
blurname <naughtybao@outlook.com>
2023-07-02 14:28:26 +0800

    BUMP: lock

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,12 +10,12 @@
     ];
 
     # nix community's cache server
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    #extra-substituters = [
+      #"https://nix-community.cachix.org"
+    #];
+    #extra-trusted-public-keys = [
+      #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    #];
   };
   inputs={
 
