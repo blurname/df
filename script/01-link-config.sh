9baa62972d98fe683c381e9836a74b5134d9d24a
blurname <naughtybao@outlook.com>
2023-07-23 12:55:59 +0800

    ADD: lazygit

--- script/01-link-config.sh
+++ script/01-link-config.sh
@@ -15,6 +15,7 @@ links=(
     "/home/bl/.xinitrc /home/bl/df/config/.config/.xinitrc"
     "/home/bl/.config/carapace /home/bl/df/config/.config/carapace"
     "/home/bl/.config/starship.toml /home/bl/df/config/.config/starship.toml"
+    "/home/bl/.config/lazygit /home/bl/df/config/.config/lazygit"
 )
 
 # 删除软链接或目标文件
