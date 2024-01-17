6c5d4812d61e573e4800bbcd6bdbf0f0189599c1
blurname <naughtybao@outlook.com>
2024-01-18 01:41:43 +0800

    CHG: nix: vscode-ssh

--- nixos/sub/editor.nix
+++ nixos/sub/editor.nix
@@ -8,6 +8,7 @@
   environment.systemPackages = with pkgs; [
     vim
     neovim
+    # openvscode-server
     #emacs
     #neovim-nightly
     #vscode
@@ -20,6 +21,20 @@
     # vimPlugins.coc-sumneko-lua
     # lua-language-server
   ];
+   # thanks to this video https://www.youtube.com/watch?v=CbDVUjbqIhc
+   # need run systemctl --user start vscode-remote-workaround
+   systemd.user = {
+    paths.vscode-remote-workaround = {
+      wantedBy = ["default.target"];
+      pathConfig.PathChanged = "%h/.vscode-server/bin";
+    };
+    services.vscode-remote-workaround.script = ''
+      for i in ~/.vscode-server/bin/*; do
+        echo "Fixing vscode-server in $i..."
+        ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+      done
+    '';
+  };
   #	 programs.neovim = {
 	#	 enable = true;
 	#	 defaultEditor = true;
