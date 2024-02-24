266820926b610b0d6ade3c6e7d4fa980e69fa517
blurname <naughtybao@outlook.com>
2024-02-25 00:21:08 +0800

    wip

--- nixos/install-flake.sh
+++ nixos/install-flake.sh
@@ -1,3 +1,10 @@
 # 安装 NixOS，设置 substituters 选项为 "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 nixos-install --flake '/home/nixos/df/nixos#nyx' --impure --option substituters "https://mirrors.bfsu.edu.cn/nix-channels/store"
-cp /home/nixos/df /mnt/etc/bl/
+echo y
+echo y
+echo y
+echo y
+
+echo b
+echo b
+cp -r /home/nixos/df /mnt/etc/bl/
