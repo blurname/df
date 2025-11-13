64d7fdd3343c8f618cbd024fb321405d8fb6ca11
blurname <naughtybao@outlook.com>
2025-11-13 12:49:19 +0800

    ADD: nixos: kde

--- nixos/sub/mod.nix
+++ nixos/sub/mod.nix
@@ -7,9 +7,13 @@
     ./language.nix
     ./boot.nix
     ./network.nix
-    ./optional/x11.nix
-    ./optional/wayland.nix
+    # ./optional/x11.nix
+    # ./optional/wayland.nix
   ];
   
   nixpkgs.config.allowUnfree = true;
+  services.xserver.enable = true; # optional
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
 }
