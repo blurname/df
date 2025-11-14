b82701670f3a29798fc2e2c91864aa504d36bee3
blurname <naughtybao@outlook.com>
2025-11-14 15:55:50 +0800

    WIP: 1

--- nixos/sub/mod.nix
+++ nixos/sub/mod.nix
@@ -13,8 +13,8 @@
   ];
   
   nixpkgs.config.allowUnfree = true;
-  services.xserver.enable = true; # optional
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.xserver.enable = true; # optional
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 }
